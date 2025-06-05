009cbd4c156d3ee2eed87b5ff2a1bb9581835a15
k-tomita20 <208903862+k-tomita20@users.noreply.github.com>
2025-06-05 18:14:05 +0000

    [Update] いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'favorite/create'
+  get 'favorite/destroy'
   root to: 'homes#top'
   
 
   devise_for :users, path: 'users'
 
   resources :users, only: [:edit, :show, :index, :update]
-  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update] do
+    resource :favorite, only: [:create, :destroy]
+  end
   get '/home/about' => 'homes#about', as: 'bookers_about'
 end
